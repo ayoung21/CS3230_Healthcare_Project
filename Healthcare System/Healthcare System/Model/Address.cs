28ba4169cb313b95be92213f5efc4741a18df9e9
Andrew Young <ayoung21@my.westga.edu>
2020-10-07 13:16:48 -0400

    created inital models for user, user role, and addres. also, renamed login form to a more appropriate name.

---
+++ Healthcare System/Healthcare System/Model/Address.cs
@@ -0,0 +1,32 @@
+﻿namespace Healthcare_System.Model
+{
+    /// <summary>
+    ///     Class to create an address for a user of the application
+    /// </summary>
+    internal class Address
+    {
+        /// <summary>
+        ///     Gets or sets the street address.
+        /// </summary>
+        /// <value>The street address.</value>
+        public string StreetAddress { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the city.
+        /// </summary>
+        /// <value>The street city.</value>
+        public string City { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the State.
+        /// </summary>
+        /// <value>The State</value>
+        public string State { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the zip code.
+        /// </summary>
+        /// <value>The zip code.</value>
+        public string Zip { get; set; }
+    }
+}
---
+++ Healthcare System/Healthcare System/Model/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace Healthcare_System.Model
+{
+    /// <summary>
+    ///     Defines the user roles for the application
+    /// </summary>
+    enum UserRoles
+    {
+        Administrator,
+        Doctor,
+        Nurse
+    }
+}
