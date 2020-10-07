d612162461077ffd10224d894cbf3a795fd04781
Andrew Young <ayoung21@my.westga.edu>
2020-10-07 13:37:07 -0400

    created configuration file for connecting to database

---
+++ Healthcare System/Healthcare System/Data Access Layer/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare_System.Data_Access_Layer
+{
+
+    /// <summary>
+    ///     Configuration file for setting up / connecting to database
+    /// </summary>
+    public static class Configuration
+    {
+        public static string DATABASE_NAME = "<INSERT DATABASE NAME HERE>";
+        public static string USER = "<INSERT USERNAME HERE>";
+        public static string PASSWORD = "<INSERT PASSWORD HERE>";
+    }
+}
