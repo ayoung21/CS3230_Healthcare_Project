786b0941fe96c858cb5523be1c21e29f55f31a99
Andrew Young <ayoung21@my.westga.edu>
2020-10-25 18:50:49 -0400

    created gui for scheduling appointment in appointments tab

--- Healthcare System/Healthcare System/View/PatientInfo.Designer.cs
+++ Healthcare System/Healthcare System/View/PatientInfo.Designer.cs
@@ -65,9 +65,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tabAppointments = new System.Windows.Forms.TabPage();
             this.tabVisits = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateTimeAppointmentDate = new System.Windows.Forms.DateTimePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.maskedTextBoxTime = new System.Windows.Forms.MaskedTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxAppointmentReasons = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.buttonScheduleAppointment = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabDetails.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.tabAppointments.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxFirstName
@@ -458,10 +470,11 @@
             // 
             // tabAppointments
             // 
+            this.tabAppointments.Controls.Add(this.groupBox2);
             this.tabAppointments.Location = new System.Drawing.Point(4, 22);
             this.tabAppointments.Name = "tabAppointments";
             this.tabAppointments.Padding = new System.Windows.Forms.Padding(3);
-            this.tabAppointments.Size = new System.Drawing.Size(592, 676);
+            this.tabAppointments.Size = new System.Drawing.Size(600, 676);
             this.tabAppointments.TabIndex = 1;
             this.tabAppointments.Text = "Appointments";
             this.tabAppointments.UseVisualStyleBackColor = true;
@@ -474,6 +487,106 @@
             this.tabVisits.TabIndex = 2;
             this.tabVisits.Text = "Visits";
             this.tabVisits.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonScheduleAppointment);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.textBoxAppointmentReasons);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.maskedTextBoxTime);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.dateTimeAppointmentDate);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(18, 324);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(563, 333);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Schedule an Appointment";
+            // 
+            // dateTimeAppointmentDate
+            // 
+            this.dateTimeAppointmentDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeAppointmentDate.Location = new System.Drawing.Point(21, 70);
+            this.dateTimeAppointmentDate.MinDate = new System.DateTime(2020, 10, 25, 0, 0, 0, 0);
+            this.dateTimeAppointmentDate.Name = "dateTimeAppointmentDate";
+            this.dateTimeAppointmentDate.Size = new System.Drawing.Size(135, 26);
+            this.dateTimeAppointmentDate.TabIndex = 0;
+            // 
+            // label11
+            // 
+            this.label11.AutoEllipsis = true;
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(17, 37);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(139, 20);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Appointment Date";
+            // 
+            // maskedTextBoxTime
+            // 
+            this.maskedTextBoxTime.Location = new System.Drawing.Point(234, 70);
+            this.maskedTextBoxTime.Mask = "00:00";
+            this.maskedTextBoxTime.Name = "maskedTextBoxTime";
+            this.maskedTextBoxTime.Size = new System.Drawing.Size(99, 26);
+            this.maskedTextBoxTime.TabIndex = 2;
+            this.maskedTextBoxTime.ValidatingType = typeof(System.DateTime);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(230, 37);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(103, 20);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Time [Military]";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(413, 37);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(57, 20);
+            this.label13.TabIndex = 4;
+            this.label13.Text = "Doctor";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(417, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // textBoxAppointmentReasons
+            // 
+            this.textBoxAppointmentReasons.Location = new System.Drawing.Point(21, 174);
+            this.textBoxAppointmentReasons.MaxLength = 200;
+            this.textBoxAppointmentReasons.Name = "textBoxAppointmentReasons";
+            this.textBoxAppointmentReasons.Size = new System.Drawing.Size(517, 26);
+            this.textBoxAppointmentReasons.TabIndex = 6;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(17, 139);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(73, 20);
+            this.label14.TabIndex = 7;
+            this.label14.Text = "Reasons";
+            // 
+            // buttonScheduleAppointment
+            // 
+            this.buttonScheduleAppointment.Location = new System.Drawing.Point(21, 252);
+            this.buttonScheduleAppointment.Name = "buttonScheduleAppointment";
+            this.buttonScheduleAppointment.Size = new System.Drawing.Size(517, 46);
+            this.buttonScheduleAppointment.TabIndex = 9;
+            this.buttonScheduleAppointment.Text = "Schedule Appointment";
+            this.buttonScheduleAppointment.UseVisualStyleBackColor = true;
             // 
             // PatientInfo
             // 
@@ -488,6 +601,9 @@
             this.tabDetails.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.tabAppointments.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -531,5 +647,15 @@
         private System.Windows.Forms.Label labelInvalidZipCode;
         private System.Windows.Forms.Label labelInvalidState;
         private System.Windows.Forms.Label labelInvalidCity;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker dateTimeAppointmentDate;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTime;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBoxAppointmentReasons;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button buttonScheduleAppointment;
     }
 }
