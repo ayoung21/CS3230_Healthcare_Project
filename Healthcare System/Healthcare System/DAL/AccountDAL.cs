f5d66be65c2d0e8d9fb75a3f5556ef3602900f55
Andrew Young <ayoung21@my.westga.edu>
2020-10-30 16:33:39 -0400

    changed authetication to use a procedure

--- Healthcare System/Healthcare System/DAL/AccountDAL.cs
+++ Healthcare System/Healthcare System/DAL/AccountDAL.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using MySql.Data.MySqlClient;
 
 namespace Healthcare_System.DAL
@@ -15,23 +16,22 @@ namespace Healthcare_System.DAL
         /// <returns>1 if the username and password are validated, 0 if not</returns>
         public static int Authenticate(string username, string password)
         {
-            string query = "SELECT COUNT(*) FROM account WHERE username = @username AND password = SHA(@password)";
-
             using (MySqlConnection connection = DbConnection.GetConnection())
             {
-                using MySqlCommand cmd = new MySqlCommand(query, connection);
-
-                cmd.Parameters.Add("@username", MySqlDbType.VarChar);
-                cmd.Parameters["@username"].Value = username;
-
-                cmd.Parameters.Add("@password", MySqlDbType.TinyText);
-                cmd.Parameters["@password"].Value = password;
-
                 connection.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                MySqlCommand cmd = new MySqlCommand("account_authenticate", connection);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                cmd.Parameters.Add(new MySqlParameter("givenusername", username));
+                cmd.Parameters.Add(new MySqlParameter("givenpassword", password));
 
                 int count = Convert.ToInt32(cmd.ExecuteScalar());
 
-                connection.Close();
                 return count;
             }
         }
