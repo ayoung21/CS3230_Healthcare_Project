234db429e33c2525005726ec0ce394320c7f7e6f
Andrew Young <ayoung21@my.westga.edu>
2020-11-14 22:09:32 -0500

    completed delete patient logic

--- Healthcare System/Healthcare System/View/MainApp.cs
+++ Healthcare System/Healthcare System/View/MainApp.cs
@@ -90,7 +90,6 @@ namespace Healthcare_System.View
 
         private void buttonDeletePatient_Click(object sender, EventArgs e)
         {
-            //TODO handle deleting patients (must make sure patient has no appointments)
             var isPatientSelected = (this.listViewPatients.SelectedItems.Count > 0) ? true : false;
 
             if (isPatientSelected)
@@ -98,10 +97,18 @@ namespace Healthcare_System.View
                 // Get patient id
                 var patientId = int.Parse(this.listViewPatients.SelectedItems[0].SubItems[3].Text);
 
-                DialogResult okToProceed = MessageBox.Show("After submitting an order, you will NOT be able to edit it!", "Please confirm you would like to continue", MessageBoxButtons.YesNo);
+                DialogResult okToProceed = MessageBox.Show("You are about to delete a patient", "Please confirm you would like to continue", MessageBoxButtons.YesNo);
                 if (okToProceed == DialogResult.Yes)
                 {
-                    
+                    if (!AppointmentDAL.PatientHasAppointment(patientId))
+                    {
+                        PatientDAL.DeletePatient(patientId);
+                        this.search();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Patient has appointments and cannot be deleted");
+                    }
                 }
             }
             else
